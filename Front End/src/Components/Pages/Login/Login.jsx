511f7c7c91c10f8abeda8d91f329a618bba983a2
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-12-07 17:47:50 +0600

    add Login with backend

--- Front End/src/Components/Pages/Login/Login.jsx
+++ Front End/src/Components/Pages/Login/Login.jsx
@@ -1,23 +1,53 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../../../apiCall/Auth/auth";
+import toast from "react-hot-toast";
+import { FaSpinner } from "react-icons/fa";
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const [user, setUser] = useState({
         email: "",
         password: "",
     })
 
-    const handleOnSubmit = (e) => {
+    const handleOnSubmit = async (e) => {
         e.preventDefault();
-        console.log(user);
+        setLoading(true);
+
+        try {
+            const response = await loginUser(user);
+
+            localStorage.setItem('token', response.data.token);
+
+            if (response.success) {
+                toast.success(response.message);
+
+                setUser({
+                    email: "",
+                    password: "",
+                })
+                navigate('/')
+            } else {
+                toast.error(response.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        } finally {
+            setLoading(false);
+        }
+
     }
 
     return (
         <div className="w-full h-screen bg-gradient-to-r from-green-400 via-teal-500 to-blue-500 flex justify-center items-center">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h2 className="text-3xl font-semibold text-center text-gray-700 mb-6">Login to Your Account</h2>
-                <form className="space-y-6">
+                <form className="space-y-6" onSubmit={handleOnSubmit}>
                     {/* Email Input */}
                     <div>
                         <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email Address</label>
@@ -49,7 +79,9 @@ const Login = () => {
                     {/* Submit Button */}
                     <div>
                         <button type="submit" className="w-full p-3 bg-teal-600 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">
-                            Login
+                            {
+                                loading ? <FaSpinner className="animate-spin mx-auto" /> : " Sign Up"
+                            }
                         </button>
                     </div>
                 </form>
--- Front End/src/apiCall/Auth/auth.js
+++ Front End/src/apiCall/Auth/auth.js
@@ -11,3 +11,13 @@ export const signUpUser = async (user) => {
         throw error;
     }
 }
+
+export const loginUser = async (user) => {
+    try {
+        const response = await axiosInstances.post("http://localhost:3000/api/users/login", user);
+        return response.data;
+    } catch (error) {
+        console.error("Error signing up user:", error);
+        throw error;
+    }
+}
